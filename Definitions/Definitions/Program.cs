d023c4944d2d5ca514660458bbce0542d485d947
nerdelt <nerdelt@gmail.com>
2018-07-25 14:14:18 -0500

    Assignment 3-3, attempt 2

--- Definitions/Definitions/Program.cs
+++ Definitions/Definitions/Program.cs
@@ -9,7 +9,8 @@ namespace Definitions
             /*  
             If you want to make an action happen in C#, you must write a statement.
             A statement can be a line or block of code, and must end with a ;
-            The following statement assigns the value "Winnie" to the string variable, dog.
+            The following statement assigns the value "Winnie" to the string variable, dog,
+            in order for it to be used later. 
             */
 
             string dog = "Winnie";
@@ -18,10 +19,9 @@ namespace Definitions
             /* 
             Expressions are lines of code which consist of operators and operands. 
             Expressions can evaluate values and invoke methods, among other things.
-            Let's take a look at the two expressions below:
-            The first expression identifies a integer variable, age, and assigns a value, 2, to it.
-            The second expression invokes the WriteLine method, which prints a message that contains 
-            variables {dog} and {age} in the console window.
+            Let's take a look at the second line below:
+            This expression invokes the WriteLine method, which prints a message that combines the  
+            variables {dog} and {age}.
             */
 
             int age = 2;
@@ -31,7 +31,7 @@ namespace Definitions
             /*
             An operator is a part of an expression that calls for an action to take place.
             In the following expression, the operators are int, =, +, and ;
-            The int initializes b as an integer variable, in order for it to be used. 
+            The int initializes b as an integer variable. 
             The = is an assignment operator, it assigns age + 1 to b;
             The + is an operator which adds age to 1.
             The ; dennotes the end of the line of code. 
